5caa5642ff6612f6a76c2788feac6c8d6edb31ee
mmy83 <mmy83@126.com>
2017-06-09 17:15:18 +0800

    测试docker api

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/astaxie/beego"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 type MainController struct {
@@ -11,5 +16,20 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Title"] = "这是一个栗子"
 	c.TplName = "index.tpl"
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
 }
